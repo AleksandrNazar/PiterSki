41c249e7c346e5a82f95050506df1ac3be689e1f
AleksandrNazar <nazarld@yandex.ru>
2023-02-06 19:20:15 +0300

    slider

--- js/script.js
+++ js/script.js
@@ -38,53 +38,6 @@ function navToggler(event) {
     }
 };
 
-//Slider
-
-const slider = document.querySelector('.slider');
-const sliderItems = Array.from(slider.children);
-const btnNext = document.querySelector('.team__btn--next');
-const btnPrev = document.querySelector('.team__btn--prev');
-
-sliderItems.forEach(function (slide, index) {
-
-    if (index !== 0) slide.classList.add('hidden');
-
-    slide.dataset.index = index;
-
-    sliderItems[0].setAttribute('data-active', '');
-
-    slide.addEventListener('click', function () {
-        showNextSlide('next');
-    });
-});
-
-btnNext.onclick = function () {
-    showNextSlide('next');
-};
-
-btnPrev.onclick = function () {
-    showNextSlide('prev');
-};
-
-function showNextSlide(direction) {
-
-    const currentSlide = slider.querySelector('[data-active]');
-    const currentSlideIndex = +currentSlide.dataset.index;
-    currentSlide.classList.add('hidden');
-    currentSlide.removeAttribute('data-active');
-
-    let nextSlideIndex;
-    if (direction === 'next') {
-        nextSlideIndex = currentSlideIndex + 1 === sliderItems.length ? 0 : currentSlideIndex + 1;
-    } else if (direction === 'prev') {
-        nextSlideIndex = currentSlideIndex === 0 ? sliderItems.length - 1 : currentSlideIndex - 1;
-    }
-
-    const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`);
-    nextSlide.classList.remove('hidden');
-    nextSlide.setAttribute('data-active', '');
-}
-
 
 //Accordion 
 const accordionItems = document.querySelectorAll('.accordion__item'); 
@@ -116,22 +69,24 @@ new Swiper('.gallery-slider', {
         disablrOnInteraction: true,
     },
 
-    slidesPerView: 3,
+    slidesPerView: 2.5,
 
     breakpoints: {
         320: {
             slidesPerView: 1,
         },
 
-        786: {
-            slidesPerView: 2,
+        768: {
+            slidesPerView: 2.2,
         },
 
         992: {
-            slidesPerView: 3,
+            slidesPerView: 2.5,
         },
     },
 });
+
+
 
 
 
